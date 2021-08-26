842060ecb458fde867ed69c909927a159873f399
Andreas Pieger <andreas.pieger@epg.com>
2021-08-26 10:29:28 +0200

    Add first checkDigit implementation

--- app.py
+++ app.py
@@ -4,6 +4,18 @@ app = Flask(__name__)
 
 def calculateCheckDigit(number):
     checkDigit = 0
+    sum = 0
+    i = 0
+
+    for digit in map(int, number):
+        i += 1
+        if i % 2 == 0:
+            sum += digit
+        else:
+            sum += digit * 3
+
+    checkDigit = 10 - sum % 10
+
     return checkDigit
 
 @app.route('/barcode/<int:number>', methods=['GET'])
@@ -12,7 +24,10 @@ def barcode():
 
 @app.route('/number/<int:number>', methods=['GET'])
 def number(number):
-    return f'Number {number}'
+    return {
+        "input": number,
+        "checkDigit": calculateCheckDigit(str(number))
+    }
 
 @app.route('/')
 def index():
