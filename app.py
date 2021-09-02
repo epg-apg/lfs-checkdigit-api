d7e928df7d98a65426696cf8008bf4b6f0bd9355
Andreas Pieger <andreas.pieger@epg.com>
2021-09-02 09:04:40 +0200

    Integrate sentry.io logging

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
-from os import abort
-from flask import Flask
+import sentry_sdk
+from flask import Flask, abort
+from sentry_sdk.integrations.flask import FlaskIntegration
 
-app = Flask(__name__)
+sentry_sdk.init(
+    dsn="https://9dbd1798137d41778fa81ba3203b92a1@o340050.ingest.sentry.io/5942394",
+    integrations=[FlaskIntegration()],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # By default the SDK will try to use the SENTRY_RELEASE
+    # environment variable, or infer a git commit
+    # SHA as release, however you may want to set
+    # something more human-readable.
+    # release="myapp@1.0.0",
+)
+app = Flask('lfs-checkdigit-api')
 
 def calculateCheckDigit(number):
     checkDigit = 0
@@ -22,7 +38,7 @@ def calculateCheckDigit(number):
 @app.route('/barcode/<int:number>', methods=['GET'])
 def barcode(number):
     if len(str(number)) != 12:
-        abort(400, "12 digits are required")
+        abort(400, '12 digits are required')
 
     return 'barcode'
 
